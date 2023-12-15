bdd52534fe9f1186202634ee18d73e93710a6155
Murilo <murilomm192@gmail.com>
2023-12-15 15:33:19 -0300

    add

---
+++ pontos.py
@@ -0,0 +1,88 @@
+import polars as pl
+import altair as alt
+import streamlit as st
+
+
+st.set_page_config('Curva Tarefas', layout='wide')
+
+@st.cache_resource
+def load_data():
+    schema = {'cod_unb': pl.Int64, 'cod_pdv': pl.Int64, 'id_sku': pl.Int64, 'segmentacao_primaria': pl.Utf8, 'task_text': pl.Utf8, 'visit_date': pl.Utf8, 'task_coins': pl.Int64, 'QTDE TASKS TOTAIS': pl.Int64, '% TASKS VALIDADAS': pl.Utf8, '% Pontos Validados': pl.Utf8} 
+    data = pl.scan_csv('pontos_bees/*.csv', separator=';', schema=schema)
+    data = data.with_columns([pl.col(x).str.replace(',','.').cast(pl.Float32) for x in ['% TASKS VALIDADAS','% Pontos Validados']])
+    cliente = pl.scan_parquet('data/cliente.parquet')
+    seg_consolidado = pl.scan_parquet('data/seg_consolidado.parquet')
+    produtos = pl.scan_parquet('data/produtos.parquet')
+    cestas = pl.scan_parquet('data/cestas.parquet')
+    
+    return data, cliente, seg_consolidado, produtos, cestas
+
+data, cliente, seg_consolidado, produtos, cestas = load_data()
+
+lista_cestas = cestas.select(pl.col('Cesta').unique().sort()).collect().to_series().to_list()
+lista_segmento = ['Todos'] + data.select(pl.col('segmentacao_primaria').unique().sort()).collect().to_series().to_list()
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.selectbox('Cestas', lista_cestas, index=lista_cestas.index('SPATEN'), key='cesta')
+with col2:
+    st.selectbox('Segmento', lista_segmento, index=lista_segmento.index('BAR'), key='seg')
+with col3:
+    col4, col5 = st.columns(2)
+    with col4:
+        st.number_input('Caixas Maior Igual:', 1, 999, 1, step=1, key='min')
+    with col5:
+        st.number_input('Caixas Menor:', 1, 999, 999, step=1, key='max')
+
+if st.session_state['seg'] != 'Todos':
+    data = data.filter(pl.col('segmentacao_primaria') == st.session_state['seg'])
+
+if st.session_state['cesta']:
+    lista_skus = cestas.filter(pl.col('Cesta') == st.session_state['cesta']).select(pl.col('cod_sku').unique().sort()).collect().to_series().to_list()
+    data = data.filter(pl.col('id_sku').is_in(lista_skus))
+    data = data.with_columns([
+        pl.when(pl.col('task_text').str.contains('de \d+ caixa'))\
+        .then(pl.col('task_text').str.extract('de (\d+) caixa',1))\
+        .otherwise(1).cast(pl.Int16).alias('cxs')
+    ]).with_columns(
+        pl.col('task_coins').qcut([x/10 for x in range(1,11)]).cast(pl.Utf8).str.extract_all('(\d+)').list.join('-').alias('bins')
+    ).with_columns(
+        pl.when(~pl.col('bins').str.contains('-'))\
+        .then(pl.concat_str([pl.lit('10-'), pl.col('bins')]))\
+        .otherwise(pl.col('bins')).alias('bins')
+    ).filter(
+        (pl.col('cxs') >= st.session_state['min']) &
+        (pl.col('cxs') < st.session_state['max'])
+    )
+    
+    data = data.group_by(['bins', 'task_coins']).agg([
+        pl.col('QTDE TASKS TOTAIS').sum().alias('quantidade'),
+        pl.col('% TASKS VALIDADAS').sum().alias('validadas'),
+        (pl.col('% TASKS VALIDADAS').sum()/pl.col('QTDE TASKS TOTAIS').sum()).alias('conversão')
+    ]).with_columns(
+        pl.col('bins').str.split('-').list[0].cast(pl.Int16).alias('sort_number')
+    ).sort('sort_number')
+
+
+final = data\
+    .filter(pl.col('conversão').is_between(0.05, 0.9))\
+    .filter(pl.col('task_coins').is_between(50, 500))\
+    .collect()\
+    .to_pandas()
+
+chart = alt.Chart(final).mark_point().encode(
+    x = alt.X('task_coins', sort=alt.SortField('sort_number')),
+    y = alt.Y('conversão').scale(zero=True),
+).properties(width = 1200, height = 600)
+
+line = chart.transform_regression('task_coins','conversão', order = 3, method='poly', extent=[50,500]).mark_line()
+
+st.altair_chart((chart + line))
+
+st.dataframe(final)
+
+    
+
+
+
+
